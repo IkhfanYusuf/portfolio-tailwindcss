7f33ca0947d2cce1c42cd973b1c5772b348a0904
ikhfan yusuf <ikhfan.yusuf@gmail.com>
2021-11-07 13:17:58 +0700

    done:build my portfolio web

---
+++ tailwind.config.js
@@ -0,0 +1,44 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    container: {
+      center: true
+    },
+    extend: {
+      backgroundColor: theme => ({
+        ...theme('colors'),
+        'green-custom' : '#52B69A',
+        'yellow-custom' : 'rgba(217, 237, 146, 0.87)',
+        'container' : '#25262A'
+      }),
+      gradientColorStops: theme => ({
+        ...theme('colors'),
+        'green-custom' : 'rgba(82, 182, 154, 1)',
+        'green-custom-500' : 'rgba(82, 182, 154, .5)',
+      }),
+      textColor: {
+        'green-custom' : "#52B69A",
+      },
+      fontSize: {
+        'h1' : ['46px', '60px'],
+        'h2' : ['30px', '45px'],
+        'h1-mobile' : ['22px', '32px'],
+        'h2-mobile' : ['16px', '24px']
+      },
+      opacity : {
+        '87' : '0.87'
+      },
+      borderColor : {
+        'green-custom' : '#52B69A',
+      },
+      ringColor: {
+        'green-custom' : "#52B69A",
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
